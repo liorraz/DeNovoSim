5f3a6902eccdf1a74d47f5e4bf60cda27546b67e
liorraz <liorraz@mail.tau.ac.il>
2017-09-04 20:14:15 +0300

    cmpl

--- deNovoZSim/src/coherence_ctrls.cpp
+++ deNovoZSim/src/coherence_ctrls.cpp
@@ -34,7 +34,7 @@
 void DeNovoImpl::init(MemObject* _parent, Network* network, const char* name){
 	info("init parent with name %s and type %s", _parent->getName(), typeid((*_parent)).name());
 	parent = _parent;
-	parentRTT = (network) ? network->getRTT(name, parent->getName()) : 0
+	parentRTT = (network) ? network->getRTT(name, parent->getName()) : 0);
 }
 
 
